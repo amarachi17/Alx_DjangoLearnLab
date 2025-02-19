ccd4b6b5c29b943fc811671f12cfb07dbcf8f263
confidence <confidencenkeonye@gmail.com>
2025-02-19 17:20:27 +0100

    Django Views and Url Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,9 @@ from .models import Book, Library
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(book_list, content_type = "text/plain")
+    return render(request, "relationship_app/list_books.html", {"books":books})
+    # book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    # return HttpResponse(book_list, content_type = "text/plain")
 
 class LibraryDetailView(DetailView):
     model = Library
