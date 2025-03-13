6b266a73fb3817e8c1e939ee30dded227d1ee174
confidence <confidencenkeonye@gmail.com>
2025-03-13 12:41:53 +0100

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/README.md
+++ advanced-api-project/README.md
@@ -18,3 +18,18 @@ This project demostrates how to create custom views and generic views using Djan
 
 - **DELETE /api/books/<id>/delete/**
     Delete an existing book (Authentication required).
+
+# Book API - Filtering, Searching, Ordering
+
+## Features
+- **Filtering**: Filters books by title, author or publication_year.
+- **Searching**: Search books by title.
+- **Ordering**: Sorts books by title or publication_year.
+
+### **1. List Books with Filters, Search, and Ordering**
+**GET /api/books/**
+
+**Filtering Examples:**
+GET /api/books/?title=The Great Gatsby
+GET /api/books/?author=John Believe
+GET /api/books/?publication_year=2021
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
@@ -8,8 +9,22 @@ from .serializers import BookSerializer
 
 # Creating a list view to retrieve all books.
 class ListView(generics.ListAPIView):
+    # API view ti retrieve all list of books with filtering, searching, and ordering capabilities.
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    # Adding filtering, searching, and ordering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    # Define filtering fields 
+    filterset_fields = ['title', 'author', 'publiction_yer']
+
+    # Enable searching on specific fields 
+    search_fields = ['title']
+
+    # Enable ordering on fields 
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title'] # Default ordering by title 
 
 # Creating a detail view to retrieve a single book by its ID.
 class DetailView(generics.RetrieveAPIView):
@@ -29,7 +44,7 @@ class UpdateView(generics.UpdateAPIView):
     permissions_classes = [permissions.IsAuthenticated] # Only authenticated users can update.
 
 # Creating a delete view to remove a book.
-class DeleteView(generics.DestoryAPIView):
+class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permissions_classes = [permissions.IsAuthenticated] # Only authenticated users can delete.
