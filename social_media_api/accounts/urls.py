92ba11063a47ced7a4581b945aa1a0d2b30c93c4
confidence <confidencenkeonye@gmail.com>
2025-03-26 17:14:25 +0100

    Implementing Posts and Comments Functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,5 +4,4 @@ from .views import RegisterUserView, LoginView
 urlpatterns = [
     path('register/', RegisterUserView, name='register'),
     path('login/', LoginView, name='login'),
-    path('profile/', )
 ]
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'rest_framework.authtoken',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -135,5 +136,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
