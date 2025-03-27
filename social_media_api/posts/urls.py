edde46216fc82e5feaab2ac482e647bbd81f5866
confidence <confidencenkeonye@gmail.com>
2025-03-27 15:02:14 +0100

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('posts/<int:post_pk>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comment-list'),
     path('posts/<int:post_pk>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='comment-detail'),
     path('feed/', FeedView.as_view(), name='user-feed'),
-    path('<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
-    path('<int:post_id>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
